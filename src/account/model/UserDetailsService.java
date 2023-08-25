90fccd49056ef57712bb5f2e5e15a6a609771e4b
EdwardKH <94784640+EdwardKHz@users.noreply.github.com>
2023-08-26 01:15:58 +0300

    Delete UserDetailsService.java

--- src/account/model/UserDetailsService.java
+++
@@ -1,9 +0,0 @@
-package account.model;
-
-import javax.naming.NameNotFoundException;
-
-public interface UserDetailsService {
-
-    UserDetails loadUserByName(String name) throws NameNotFoundException;
-    UserDetails loadUserByEmail(String email) throws NameNotFoundException;
-}
